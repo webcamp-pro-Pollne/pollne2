c2e88709fa3b518644bf9c9f747d8fddf8fb27c3
EC2 Default User <ec2-user@ip-172-31-35-225.ec2.internal>
2021-03-16 04:22:47 +0000

    Sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, controllers: {
+    sessions: "customers/sessions"
+  }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
