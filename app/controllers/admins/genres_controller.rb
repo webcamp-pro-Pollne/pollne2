c3b79d65847e855de7e934c6db23ba17f9ca24c1
“Ejima-we” <takumi941503@gmail.com>
2021-03-19 05:15:09 +0000

    genre機能追加

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,15 +1,25 @@
 class Admins::GenresController < ApplicationController
-  
+
   def index
+    @genres = Genre.all
+    @genre = Genre.new
   end
-  
+
   def edit
   end
-  
+
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_back(fallback_location: root_path)
   end
-  
+
   def update
   end
-  
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
