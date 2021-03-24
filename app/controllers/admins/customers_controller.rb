d87c0e8e05b40455a54c640393fd2dfa3ee3db42
EisukeTochitani <kop.michael1958@gmail.com>
2021-03-24 07:34:15 +0000

    [add]admin側会員編集画面追加

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,34 +1,36 @@
 class Admins::CustomersController < ApplicationController
    before_action :authenticate_admin!
-  
+
   def index
-    @customers = Customer.page(params[:page]).per(2)
+    @customers = Customer.all.page(params[:page]).per(5)
   end
-  
+
   def show
-    @customer  = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
-  
+
   def edit
-     @customer  = Customer.find(params[:id])
+  	@customer = Customer.find(params[:id])
   end
-  
-  
+
   def update
-    @customer  = Customer.find(params[:id])
-    if @customer.update(cutomer_params)
-       redirect_to adimis_customer_path(@customer), motice: "顧客情報を変更しました"
+   @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:success] = "会員情報を変更しました"
+      redirect_to admins_customer_path
     else
-      render :edit
+      render "edit"
     end
   end
-  
+
+
+
 
 
  private
-  
-  def cutomer_params
+
+  def customer_params
    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :adress, :telephone_number, :email, :is_deleted)
   end
- 
+
 end
