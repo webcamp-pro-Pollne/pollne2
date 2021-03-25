190caf0569c325ebc4f297b6f67ada2beb931d09
EC2 Default User <ec2-user@ip-172-31-38-194.ec2.internal>
2021-03-25 05:45:58 +0000

    Admin/order

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,13 +1,22 @@
 class Admins::OrdersController < ApplicationController
 
   def top
-    @orders = Order.all
+    @orders = Order.page(params[:page]).reverse_order
   end
 
   def show
+    @order = Order.find(params[:id])
+    @orders_details = @order.orders_details
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_back(fallback_location: root_path)
   end
 
+  private
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
--- app/controllers/admins/orders_details_controller.rb
+++ app/controllers/admins/orders_details_controller.rb
@@ -1,6 +1,13 @@
 class Admins::OrdersDetailsController < ApplicationController
-  
+
   def update
+    @orders_detail = OrdersDetail.find(params[:id])
+    @orders_detail.update(orders_detail_params)
+    redirect_to admins_order_path(@orders_detail.order)
   end
-  
+
+  private
+  def orders_detail_params
+  params.require(:orders_detail).permit(:making_status)
+  end
 end
